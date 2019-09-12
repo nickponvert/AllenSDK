ea2b85a618f2cecb9d8f7e056c4800b22184967c
Nick Ponvert <nickponvert@gmail.com>
2019-09-12 16:01:05 -0700

    WIP SFN behavior cache

--- allensdk/brain_observatory/behavior/behavior_project_cache.py
+++ allensdk/brain_observatory/behavior/behavior_project_cache.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from functools import partial
 from allensdk.api.cache import Cache
 from allensdk.brain_observatory.ecephys.ecephys_project_cache import call_caching
 from allensdk.brain_observatory.ecephys.file_promise import write_from_stream
@@ -6,6 +7,8 @@ from allensdk.brain_observatory.behavior.behavior_project_api.behavior_project_f
 from allensdk.brain_observatory.behavior.behavior_project_api.behavior_project_lims_api import BehaviorProjectLimsApi
 from allensdk.brain_observatory.behavior.behavior_ophys_api.behavior_ophys_nwb_api import BehaviorOphysNwbApi, ExtendedBehaviorOphysNwbApi
 from allensdk.brain_observatory.behavior.behavior_ophys_session import BehaviorOphysSession, ExtendedBehaviorOphysSession
+from allensdk.internal.api.behavior_ophys_api import BehaviorOphysLimsApi
+from allensdk.brain_observatory.mesoscope import mesoscope_session as meso
 
 csv_io = {
     'reader': lambda path: pd.read_csv(path, index_col='Unnamed: 0'),
@@ -35,9 +38,10 @@ class BehaviorProjectCache(Cache):
         super(BehaviorProjectCache, self).__init__(**kwargs)
         self.fetch_api = fetch_api
 
-    def get_sessions(self):
+    def get_sessions(self, **get_sessions_kwargs):
         path = self.get_cache_path(None, self.SESSIONS_KEY)
-        return call_caching(self.fetch_api.get_sessions, path=path, strategy='lazy', writer=csv_io["writer"], reader=csv_io['reader'])
+        return call_caching(partial(self.fetch_api.get_sessions, **get_sessions_kwargs),
+                            path=path, strategy='lazy', writer=csv_io["writer"], reader=csv_io['reader'])
 
     def get_session_data(self, experiment_id):
         # TODO: Use session ID here? will need to specify per-plane if we want to support mesoscope though, which would be experiment ID
@@ -131,3 +135,93 @@ class BehaviorProjectCache(Cache):
     @classmethod
     def fixed(cls, **kwargs):
         return cls(fetch_api=BehaviorProjectFixedApi(), **kwargs)
+
+class InternalCacheFromLims(BehaviorProjectCache):
+
+    def __init__(self):
+        fetch_api = BehaviorProjectLimsApi.default()
+        self.cache=False
+        super().__init__(fetch_api)
+        self.SCIENTIFICA_RIG_NAMES = ['CAM2P.3', 'CAM2P.4', 'CAM2P.5']
+        self.MESOSCOPE_RIG_NAMES = ['MESO.1']
+
+    def get_sessions(self, **get_sessions_kwargs):
+        '''
+        Bypass call_caching so we can be flexible with kwargs to this call for now. 
+        '''
+        return self.fetch_api.get_sessions(**get_sessions_kwargs)
+    
+    def get_session(self, ophys_session_id):
+        # Return regular session from lims if scientifica, otherwise return mesoscope session
+        experiment_table = self.get_sessions() #Actually returns experiments
+        experiments_this_session_id = experiment_table.query("ophys_session_id == @ophys_session_id")
+
+        if len(experiments_this_session_id)>0:
+            equipment_name = experiments_this_session_id['equipment_name'].unique()
+            assert len(equipment_name)==1
+            equipment_name = equipment_name[0]
+
+            if equipment_name in self.SCIENTIFICA_RIG_NAMES:
+                assert len(experiments_this_session_id)==1
+                ophys_experiment_id = experiments_this_session_id.iloc[0]['ophys_experiment_id']
+                api = BehaviorOphysLimsApi(ophys_experiment_id)
+                session = BehaviorOphysSession(api)
+
+            elif equipment_name in self.MESOSCOPE_RIG_NAMES:
+                #  assert len(experiments_this_session_id)==8
+                session = meso.MesoscopeSession.from_lims(ophys_session_id)
+
+            else:
+                raise ValueError("Unknown equipment name: {}".format(equipment_name))
+
+        else:
+            raise ValueError("Session ID not in the cache")
+
+        return session
+
+    def get_behavior_only_session(behavior_session_id):
+        pass
+
+
+    def get_all_behavior_sessions(donor_id,
+                                  exclude_imaging_sessions = False,
+                                  imaging_rigs = ['CAM2P.3', 'CAM2P.4', 'CAM2P.5']):
+        postgres_api = PostgresQueryMixin()
+        all_behavior_sessions_query = '''
+        SELECT
+        vbc.id as container_id,
+        vbc.workflow_state as container_qc_state,
+        d.id as donor_id,
+        bs.id as behavior_session_id,
+        bs.created_at,
+        bs.foraging_id,
+        e.name as equipment_name,
+        os.id as ophys_session_id,
+        os.name as ophys_session_name,
+        oe.id as ophys_experiment_id,
+        oe.workflow_state as ophys_qc_state,
+        p.name as project_name,
+        imaging_depths.depth as imaging_depth,
+        st.acronym as targeted_structure
+        FROM visual_behavior_experiment_containers vbc
+        JOIN specimens sp ON sp.id=vbc.specimen_id
+        JOIN donors d ON d.id=sp.donor_id
+        LEFT JOIN behavior_sessions bs ON bs.donor_id = d.id
+        LEFT JOIN equipment e on e.id = bs.equipment_id
+        LEFT JOIN ophys_sessions os on os.foraging_id = bs.foraging_id
+        LEFT JOIN projects p ON p.id=os.project_id
+        LEFT JOIN ophys_experiments oe on oe.ophys_session_id = os.id
+        LEFT JOIN structures st ON st.id=oe.targeted_structure_id
+        LEFT JOIN imaging_depths ON imaging_depths.id=oe.imaging_depth_id
+        WHERE d.id = {}
+        '''.format(donor_id)
+
+        all_sessions = pd.read_sql(all_behavior_sessions_query, postgres_api.get_connection())
+        if exclude_imaging_sessions:
+            all_sessions = all_sessions.query('equipment_name not in @imaging_rigs')
+        all_sessions = all_sessions.sort_values('created_at').reset_index(drop=True)
+        return all_sessions
+
+if __name__=="__main__":
+    cache = InternalCacheFromLims()
+
